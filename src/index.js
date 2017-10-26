79f794156dc42269f35414721dadf254581b4043
Aiden Cheung <aiden.cheung@hotmail.com>
2017-10-26 11:28:36 +0100

    blah

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Counter extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+        this.handleIncrement = this.handleIncrement.bind(this);
+        this.handleD = this.handleD.bind(this);
+        this.handleChange = this.handleChangec.bind(this);
+    }
+    handleIncrement () {
+        this.setState({
+            value: this.state.value + 1
+        });
+    }
+    handleD () {
+
+        if(this.state.value < 1){
+            return
+        }
+        this.setState({
+            value: this.state.value - 1
+        });
+    }
+    handleChange (event) {
+        if(event.target.innerHTML === '+'){
+            this.setState({
+                value: this.state.value + 1
+            });
+        }else{
+            if(this.state.value === 0){
+                return;
+            }
+            this.setState({
+                value: this.state.value - 1
+            });
+        }
+    }
+    render () {
+        return (
+            <div className="counter">
+                {this.props.title}
+                {": "}
+                <span className="counter-value">{this.state.value}</span>
+                <button onClick={this.handleChange}>+</button>
+                <button onClick={this.handleChange}>-</button>
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(
+    <Counter title="abc"/>,
+    document.getElementById('root')
+);
